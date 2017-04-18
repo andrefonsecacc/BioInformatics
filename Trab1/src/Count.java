3e0780b11ae5006ed778bb10fdb1c5f98b169d1f
andrefonsecacc <andre.j.fonseca@gmail.com>
2017-04-18 18:44:11 +0100

    2 first points are done

---
+++ Trab1/src/Count.java
@@ -0,0 +1,123 @@
+
+public class Count {
+
+	/**
+	 * Method to count nucleotides(A,C,G,T) in a sequence
+	 * 
+	 * @param bases
+	 * @param sequence
+	 */
+	public static void count_bases_DNA(String[] bases, String[] sequence) {
+
+		int count_A = 0, count_C = 0, count_G = 0, count_T = 0;
+
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i].equals(bases[0])) {
+				count_A++;
+			}
+			if (sequence[i].equals(bases[1])) {
+				count_C++;
+			}
+			if (sequence[i].equals(bases[2])) {
+				count_G++;
+			}
+			if (sequence[i].equals(bases[3])) {
+				count_T++;
+			}
+		}
+		System.out.println("Base A: " + count_A + "\n" + "Base C: " + count_C + "\n" + "Base G: " + count_G + "\n"
+				+ "Base T: " + count_T + "\n");
+
+	}
+
+	/**
+	 * Method to count nucleotides(A,C,G,U) in a sequence
+	 * 
+	 * @param bases
+	 * @param sequence
+	 */
+	public static void count_bases_RNA(String[] bases, String[] sequence) {
+
+		int count_A = 0, count_C = 0, count_G = 0, count_U = 0;
+
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i].equals(bases[0])) {
+				count_A++;
+			}
+			if (sequence[i].equals(bases[1])) {
+				count_C++;
+			}
+			if (sequence[i].equals(bases[2])) {
+				count_G++;
+			}
+			if (sequence[i].equals(bases[3])) {
+				count_U++;
+			}
+
+			
+
+		}
+		System.out.println("Base A: " + count_A + "\n" + "Base C: " + count_C + "\n" + "Base G: " + count_G + "\n"
+				+ "Base U: " + count_U + "\n");
+
+		
+
+	}
+
+	/**
+	 * Print some DNA or RNA array sequence
+	 * 
+	 * @param arr
+	 */
+	public static void printSequence(String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == null) {
+				break;
+			}
+			System.out.print(arr[i]);
+		}
+	}
+
+	/**
+	 * Converts DNA to RNA
+	 * 
+	 * @param bases
+	 * @param sequence
+	 *            of DNA
+	 */
+	public static String[] DNAtoRNA(String[] bases, String[] sequence) {
+		
+		int k=0;
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i].equals(bases[0]) || sequence[i].equals(bases[1]) || sequence[i].equals(bases[2])
+					|| sequence[i].equals(bases[3])) {
+				k++;
+				}
+			}
+		System.out.println(k);
+			
+		int j = 0;
+		String[] arr = new String[k];
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i].equals(bases[0]) || sequence[i].equals(bases[1]) || sequence[i].equals(bases[2])
+					|| sequence[i].equals(bases[3])) {
+
+				if (sequence[i].equals(bases[3])) {
+					arr[j] = "U";
+					j++;
+				} else {
+					arr[j] = sequence[i];
+					j++;
+				}
+
+			}
+		}
+		System.out.print("DNAtoRNA: ");
+		printSequence(arr);
+		System.out.println();
+		return arr;
+
+	}
+	
+	
+}
--- Trab1/src/Nucleo.java
+++ Trab1/src/Nucleo.java
@@ -19,14 +19,12 @@ public class Nucleo {
 	private static String[] sequence = null;
 	private static String[] sequence2 = null;
 
-	private static String[] bases = { "A", "C", "G", "T" };
+	private static String[] sequence_rna = null;
+	private static String[] sequence_rna_2=null;
 
-	private static String[] startCodon = { "A", "U", "G" };
-	private static String[] endCodon = { "U", "A", "A" };
-	private static String[] endCodon_2 = { "U", "A", "G" };
-	private static String[] endCodon_3 = { "U", "G", "A" };
+	private static String[] bases_dna = { "A", "C", "G", "T" };
 
-	private static int ReadingFrame = 3;
+	private static String[] bases_rna = { "A", "C", "G", "U" };
 
 	/**
 	 * Method for string to string array conversion
@@ -42,77 +40,6 @@ public class Nucleo {
 		return arr;
 	}
 
-	/**
-	 * Print some DNA or RNA array sequence
-	 * 
-	 * @param arr
-	 */
-	public static void printSequence(String[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == null) {
-				break;
-			}
-			System.out.print(arr[i]);
-		}
-	}
-
-	/**
-	 * Method to count nucleotides(A,C,G,T) in a sequence
-	 * 
-	 * @param bases
-	 * @param sequence
-	 */
-	public static void count_bases(String[] bases, String[] sequence) {
-
-		int count_A = 0, count_C = 0, count_G = 0, count_T = 0;
-
-		for (int i = 0; i < sequence.length; i++) {
-			if (sequence[i].equals(bases[0])) {
-				count_A++;
-			}
-			if (sequence[i].equals(bases[1])) {
-				count_C++;
-			}
-			if (sequence[i].equals(bases[2])) {
-				count_G++;
-			}
-			if (sequence[i].equals(bases[3])) {
-				count_T++;
-			}
-		}
-		System.out.println("Base A: " + count_A + "\n" + "Base C: " + count_C + "\n" + "Base G: " + count_G + "\n"
-				+ "Base T: " + count_T + "\n");
-
-	}
-
-	/**
-	 * Converts DNA to RNA
-	 * 
-	 * @param bases
-	 * @param sequence
-	 *            of DNA
-	 */
-	private static void DNAtoRNA(String[] bases, String[] sequence) {
-		int j = 0;
-		String[] arr = new String[sequence.length];
-		for (int i = 0; i < sequence.length; i++) {
-			if (sequence[i].equals(bases[0]) || sequence[i].equals(bases[1]) || sequence[i].equals(bases[2])
-					|| sequence[i].equals(bases[3])) {
-				if (sequence[i].equals(bases[3])) {
-					arr[j] = "U";
-					j++;
-				} else {
-					arr[j] = sequence[i];
-					j++;
-				}
-			}
-		}
-
-		System.out.print("DNAtoRNA: ");
-		printSequence(arr);
-		System.out.println();
-	}
-
 	public static void main(String args[]) throws IOException {
 
 		Eucariote = new Reader(Euca);
@@ -121,11 +48,20 @@ public class Nucleo {
 		sequence = explode(Eucariote.getSequence());
 		sequence2 = explode(Influenza.getSequence());
 
-		count_bases(bases, sequence);
-		count_bases(bases, sequence2);
+		/**
+		 * totais de nucleotideos no genome
+		 * 
+		 */
+		Count.count_bases_DNA(bases_dna, sequence);
+		Count.count_bases_DNA(bases_dna, sequence2);
 
-		DNAtoRNA(bases, sequence);
-		DNAtoRNA(bases,sequence2);
+		/**
+		 * totais de nucleotideos no material codificador
+		 */
+		sequence_rna = Count.DNAtoRNA(bases_dna, sequence);
+		Count.count_bases_RNA(bases_rna, sequence_rna);
+		sequence_rna_2=Count.DNAtoRNA(bases_dna, sequence2);
+		Count.count_bases_RNA(bases_rna, sequence_rna_2);
+
 	}
-
 }
